94c35ca7e167a5e412fad43f5b8545b5be894476
yiliz <yilizhang3@gmail.com>
2026-07-07 19:04:24 +0200

    delete chatbot

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { useContext } from 'react';
-import { Nav } from '../components/Navbar';
-import { Intro, Presentation, About } from '../components/Intro';
-import { Projects } from '../components/Projects';
-import { Footer, Contact } from '../components/Footer';
-import { about, contact, intro, navigation, projects, SEO, presentation } from '../config/config';
+import React, { Fragment, useEffect, useState } from "react";
+import { useContext } from "react";
+import { Nav } from "../components/Navbar";
+import { Intro, Presentation, About } from "../components/Intro";
+import { Projects } from "../components/Projects";
+import { Footer, Contact } from "../components/Footer";
+import {
+  about,
+  contact,
+  intro,
+  navigation,
+  projects,
+  SEO,
+  presentation,
+} from "../config/config";
 import {
   about_en,
   contact_en,
@@ -13,19 +21,19 @@ import {
   projects_en,
   SEO_en,
   presentation_en,
-} from '../config/config_en';
-import { MyHead } from '../components/MyHead';
-import { Chat } from '@/components/Chat';
-import { LanguageContext } from '../components/LanguageContext';
-import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
-import Image from 'next/image';
-import p from '../assets/image/entry.png';
-import { motion } from 'framer-motion';
-import useResponsivePanelSize from '@/hooks/useResponsivePanelSize';
+} from "../config/config_en";
+import { MyHead } from "../components/MyHead";
+import { Chat } from "@/components/Chat";
+import { LanguageContext } from "../components/LanguageContext";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import Image from "next/image";
+import p from "../assets/image/entry.png";
+import { motion } from "framer-motion";
+import useResponsivePanelSize from "@/hooks/useResponsivePanelSize";
 export default function Home() {
   const { language } = useContext(LanguageContext);
   const content =
-    language === 'en'
+    language === "en"
       ? {
           about: about_en,
           contact: contact_en,
@@ -56,11 +64,14 @@ export default function Home() {
   const maxSizeRight = isMobile ? MOBILE_RIGHT_WIDTH : 50;
   return (
     <div className="position-relative">
-      <PanelGroup direction="horizontal" style={{ height: '100vh' }}>
-        <Panel defaultSize={100} style={{ height: '100%', overflowY: 'auto' }}>
+      <PanelGroup direction="horizontal" style={{ height: "100vh" }}>
+        <Panel defaultSize={100} style={{ height: "100%", overflowY: "auto" }}>
           <Fragment>
             <MyHead seo={content.SEO} />
-            <Nav title={content.navigation.name} links={content.navigation.links} />
+            <Nav
+              title={content.navigation.name}
+              links={content.navigation.links}
+            />
             <Intro
               title={content.intro.title}
               description={content.intro.description}
@@ -70,14 +81,17 @@ export default function Home() {
               title={content.presentation.title}
               description={content.presentation.description}
             />
-            <Projects title={content.projects.title} cards={content.projects.cards} />
+            <Projects
+              title={content.projects.title}
+              cards={content.projects.cards}
+            />
             <About title={content.about.title} cards={content.about.cards} />
             <Contact
               title={content.contact.title}
               description={content.contact.description}
               buttons={content.contact.buttons}
             />
-            <button
+            {/* <button
               id="dynamicBtn"
               className={'position-fixed top-50 end-0 translate-middle-y border-0 bg-transparent'}
               onClick={() => setIsOpen(true)}
@@ -97,16 +111,23 @@ export default function Home() {
               >
                 <Image src={p.src} alt={'chatbot'} width={100} height={100} />
               </motion.div>
-            </button>
+            </button> */}
             <Footer />
           </Fragment>
         </Panel>
         {isOpen && (
           <>
             <PanelResizeHandle />
-            <Panel defaultSize={rightPanelSize} minSize={minSizeRight} maxSize={maxSizeRight}>
+            <Panel
+              defaultSize={rightPanelSize}
+              minSize={minSizeRight}
+              maxSize={maxSizeRight}
+            >
               <div className="d-flex justify-content-center w-100 h-100 p-3">
-                <button className={'position-fixed top-0 end-0'} onClick={() => setIsOpen(false)}>
+                <button
+                  className={"position-fixed top-0 end-0"}
+                  onClick={() => setIsOpen(false)}
+                >
                   ✕
                 </button>
                 <Chat />
